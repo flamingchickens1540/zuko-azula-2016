65e3ccb71b760c8ca806eac073ea9ac64dadd6bf
Tytynguyen <tytynguyens2000@yahoo.com>
2016-01-16 12:07:24 -0800

    Added publishing

--- ZukoAzula2016/src/org/team1540/zukoazula/DriveCode.java
+++ ZukoAzula2016/src/org/team1540/zukoazula/DriveCode.java
@@ -10,21 +10,23 @@ import ccre.frc.FRC;
 
 public class DriveCode {
 
-    private static final FloatInput pilotLeftAxis = FRC.joystick1.axis(2).deadzone(0.1f), pilotRightAxis = FRC.joystick1.axis(6).deadzone(0.1f);
+    private static final FloatInput pilotLeftAxis = FRC.joystick1.axis(2).deadzone(0.2f), pilotRightAxis = FRC.joystick1.axis(6).deadzone(0.2f);
     private static final FloatInput pilotRightTrigger = FRC.joystick1.axis(4).deadzone(0.1f), pilotLeftTrigger = FRC.joystick1.axis(3).deadzone(0.1f);
     
-    private static final ExtendedMotor leftFrontCAN = FRC.talonCAN(0), leftBackCAN = FRC.talonCAN(1), rightFrontCAN = FRC.talonCAN(2), rightBackCAN = FRC.talonCAN(3);
+    private static final ExtendedMotor rightFrontCAN = FRC.talonCAN(2), rightBackCAN = FRC.talonCAN(3), leftFrontCAN = FRC.talonCAN(0), leftBackCAN = FRC.talonCAN(1); 
     
     public static void initDriveCode() throws ExtendedMotorFailureException {
 
         final FloatOutput leftMotors = leftFrontCAN.simpleControl().combine(leftBackCAN.simpleControl());
         final FloatOutput rightMotors = rightFrontCAN.simpleControl(true).combine(rightBackCAN.simpleControl(true));
 
-        FloatOutput leftOut = leftMotors.addRamping(0.1f, FRC.constantPeriodic);
-        FloatOutput rightOut = rightMotors.addRamping(0.1f, FRC.constantPeriodic);
+        final FloatOutput leftOut = leftMotors.addRamping(0.1f, FRC.constantPeriodic);
+        final FloatOutput rightOut = rightMotors.addRamping(0.1f, FRC.constantPeriodic);
 
         Drive.extendedTank(pilotLeftAxis, pilotRightAxis, pilotLeftTrigger.minus(pilotRightTrigger), leftOut, rightOut);
         
+        Cluck.publish("Left Axis", pilotLeftAxis);
+        Cluck.publish("Right Axis", pilotRightAxis);
         Cluck.publish("Left Output", leftOut);
         Cluck.publish("Right Output", rightOut);
     }
--- ZukoAzula2016/src/org/team1540/zukoazula/RobotTemplate.java
+++ ZukoAzula2016/src/org/team1540/zukoazula/RobotTemplate.java
@@ -4,6 +4,7 @@ import ccre.channel.BooleanCell;
 import ccre.channel.BooleanOutput;
 import ccre.channel.FloatOutput;
 import ccre.ctrl.Drive;
+import ccre.ctrl.ExtendedMotorFailureException;
 import ccre.frc.FRC;
 import ccre.frc.FRCApplication;
 import ccre.log.Logger;
@@ -13,13 +14,14 @@ public class RobotTemplate implements FRCApplication {
     public static final int TEAM_NUMBER = 1540;
 
     @Override
-    public void setupRobot() {
+    public void setupRobot() throws ExtendedMotorFailureException {
         Logger.info("ðŸ�£ CHEEP CHEEP ðŸ�£");
 
-        // Currently for Valkyrie
-        FloatOutput left = FRC.talon(4, FRC.MOTOR_FORWARD).combine(FRC.talon(5, FRC.MOTOR_REVERSE)).combine(FRC.talon(6, FRC.MOTOR_FORWARD));
-        FloatOutput right = FRC.talon(1, FRC.MOTOR_REVERSE).combine(FRC.talon(2, FRC.MOTOR_REVERSE)).combine(FRC.talon(3, FRC.MOTOR_REVERSE));
-        Drive.tank(FRC.joystick1.axisY().negated(), FRC.joystick1.axis(6).negated(), left, right);
-        new BooleanCell(FRC.solenoid(1)).toggleWhen(FRC.joystick1.onPress(1));
+        DriveCode.initDriveCode();
+//        // Currently for Valkyrie
+//        FloatOutput left = FRC.talon(4, FRC.MOTOR_FORWARD).combine(FRC.talon(5, FRC.MOTOR_REVERSE)).combine(FRC.talon(6, FRC.MOTOR_FORWARD));
+//        FloatOutput right = FRC.talon(1, FRC.MOTOR_REVERSE).combine(FRC.talon(2, FRC.MOTOR_REVERSE)).combine(FRC.talon(3, FRC.MOTOR_REVERSE));
+//        Drive.tank(FRC.joystick1.axisY().negated(), FRC.joystick1.axis(6).negated(), left, right);
+//        new BooleanCell(FRC.solenoid(1)).toggleWhen(FRC.joystick1.onPress(1));
     }
 }
