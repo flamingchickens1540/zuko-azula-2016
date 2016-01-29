9758d4e11e466ac58429332faf739c9b5454c58e
AidanSmith <smithai@catlin.edu>
2016-01-28 16:05:53 -0800

    Removed autonomous code and inlined stuff

--- ZukoAzula2016/src/org/team1540/zukoazula/DriveCode.java
+++ ZukoAzula2016/src/org/team1540/zukoazula/DriveCode.java
@@ -22,39 +22,26 @@ public class DriveCode {
     private static final ExtendedMotor rightFrontCAN = FRC.talonCAN(2), rightBackCAN = FRC.talonCAN(3);
     private static final ExtendedMotor leftFrontCAN = FRC.talonCAN(0), leftBackCAN = FRC.talonCAN(1);
 
-    public static final FloatCell autoLeftMotors = new FloatCell();
-    public static final FloatCell autoRightMotors = new FloatCell();
-    public static final FloatOutput autoAllMotors = autoLeftMotors.combine(autoRightMotors);
-
     private static final BehaviorArbitrator behaviors = new BehaviorArbitrator("Drive Code");
     private static final ArbitratedFloat leftInput = behaviors.addFloat();
     private static final ArbitratedFloat rightInput = behaviors.addFloat();
     private static final Behavior teleop = behaviors.addBehavior("Teleop", FRC.inTeleopMode());
-    private static final Behavior auto = behaviors.addBehavior("Autonomous", FRC.inAutonomousMode());
     private static final Behavior pit = behaviors.addBehavior("Pit Mode", ZukoAzula.mainTuning.getBoolean("Pit Mode Enable", false));
 
     public static void setup() throws ExtendedMotorFailureException {
         leftInput.attach(teleop, driveLeftAxis.plus(driveLeftTrigger.minus(driveRightTrigger)));
         rightInput.attach(teleop, driveRightAxis.plus(driveRightTrigger.minus(driveLeftTrigger)));
-        leftInput.attach(auto, autoLeftMotors);
-        rightInput.attach(auto, autoRightMotors);
         leftInput.attach(pit, FloatInput.zero);
         rightInput.attach(pit, FloatInput.zero);
 
-        final FloatOutput leftMotors = leftFrontCAN.simpleControl(FRC.MOTOR_FORWARD).combine(leftBackCAN.simpleControl(FRC.MOTOR_FORWARD));
-        final FloatOutput rightMotors = rightFrontCAN.simpleControl(FRC.MOTOR_REVERSE).combine(rightBackCAN.simpleControl(FRC.MOTOR_REVERSE));
-
-        final FloatOutput leftOut = leftMotors.addRamping(0.1f, FRC.constantPeriodic);
-        final FloatOutput rightOut = rightMotors.addRamping(0.1f, FRC.constantPeriodic);
-
-        leftInput.send(leftOut);
-        rightInput.send(rightOut);
+        leftInput.send(leftFrontCAN.simpleControl(FRC.MOTOR_FORWARD).combine(leftBackCAN.simpleControl(FRC.MOTOR_FORWARD)).addRamping(0.1f, FRC.constantPeriodic));
+        rightInput.send(rightFrontCAN.simpleControl(FRC.MOTOR_REVERSE).combine(rightBackCAN.simpleControl(FRC.MOTOR_REVERSE)).addRamping(0.1f, FRC.constantPeriodic));
 
         Cluck.publish("Drive Left Raw", driveLeftAxis);
         Cluck.publish("Drive Right Raw", driveRightAxis);
         Cluck.publish("Drive Forwards Raw", driveRightTrigger);
         Cluck.publish("Drive Backwards Raw", driveLeftTrigger);
-        Cluck.publish("Drive Left Output", leftOut);
-        Cluck.publish("Drive Right Output", rightOut);
+        Cluck.publish("Drive Left Motors", leftInput);
+        Cluck.publish("Drive Right Motors", rightInput);
     }
 }
