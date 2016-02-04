3425b3b3d1ad768eb6376486a5114a6eb9782276
Alexander Mackworth <alexander@tssr.co>
2016-02-03 17:25:17 -0800

    Initial intake arm implementation.

---
+++ ZukoAzula2016/src/org/team1540/zukoazula/IntakeArm.java
@@ -0,0 +1,28 @@
+package org.team1540.zukoazula;
+
+import ccre.channel.BooleanIO;
+import ccre.channel.BooleanInput;
+import ccre.channel.EventInput;
+import ccre.channel.FloatInput;
+import ccre.channel.FloatOutput;
+import ccre.ctrl.ExtendedMotor;
+import ccre.ctrl.ExtendedMotorFailureException;
+import ccre.drivers.ctre.talon.TalonExtendedMotor;
+import ccre.frc.FRC;
+
+public class IntakeArm {
+    private static final TalonExtendedMotor intakeArmCAN = FRC.talonCAN(9);
+    private static final TalonExtendedMotor intakeArmRollerCAN = FRC.talonCAN(11);
+    private static final FloatInput encoder = intakeArmCAN.modEncoder().getEncoderVelocity();
+
+    private static final FloatInput intakeArmAxis = ZukoAzula.controlBinding.addFloat("Intake Arm Axis").deadzone(0.2f).negated();
+    private static final BooleanInput intakeArmRollerToggle = ZukoAzula.controlBinding.addToggleButton("Intake Arm Rollers Toggle");
+
+    public static void setupIntakeArm() throws ExtendedMotorFailureException {
+        intakeArmAxis.send(intakeArmCAN.simpleControl());
+        FloatOutput rollerOutput = intakeArmRollerCAN.simpleControl();
+        intakeArmRollerToggle.send((value) -> {
+            rollerOutput.set(value ? 1 : 0);
+        });
+    }
+}
