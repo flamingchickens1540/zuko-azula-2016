703f8b7c250b04fdd8e0f21bc0fcd48a34bde758
Colby Skeggs <skeggsc@catlin.edu>
2016-02-03 18:18:29 -0800

    Refactor Shooter to also include rollers and sequencing

--- ZukoAzula2016/src/org/team1540/zukoazula/Shooter.java
+++ ZukoAzula2016/src/org/team1540/zukoazula/Shooter.java
@@ -2,6 +2,9 @@ package org.team1540.zukoazula;
 
 import ccre.behaviors.ArbitratedBoolean;
 import ccre.behaviors.ArbitratedFloat;
+import ccre.behaviors.Behavior;
+import ccre.behaviors.BehaviorArbitrator;
+import ccre.channel.BooleanCell;
 import ccre.channel.BooleanInput;
 import ccre.channel.DerivedFloatInput;
 import ccre.channel.EventCell;
@@ -15,21 +18,35 @@ import ccre.ctrl.ExtendedMotorFailureException;
 import ccre.drivers.ctre.talon.TalonExtendedMotor;
 import ccre.frc.FRC;
 import ccre.time.Time;
+import ccre.timers.ExpirationTimer;
+import ccre.timers.PauseTimer;
 
 public class Shooter {
+    private static final BehaviorArbitrator rollerArb = new BehaviorArbitrator("Rollers");
+    private static final ArbitratedFloat rollerSpeed = rollerArb.addFloat();
+
     public static void setup() throws ExtendedMotorFailureException {
-        TalonExtendedMotor talons = makeTalons();
+        rollerSpeed.send(FRC.talonSimpleCAN(8, FRC.MOTOR_FORWARD));
 
-        FloatOutput motors = talons.simpleControl();
-        Cluck.publish("Shooter Motors", motors);
+        BangBangTalon shooter = new BangBangTalon(makeLinkedTalons(), "Shooter");
 
-        FloatInput velocity = talons.modEncoder().getEncoderVelocity();
-        Cluck.publish("Shooter Velocity", velocity);
+        // passively roll to the wheel in teleop, while the shooter is stopped.
+        rollerSpeed.attach(rollerArb.addBehavior("Passive Roll", FRC.inTeleopMode().and(shooter.isStopped)), ZukoAzula.mainTuning.getFloat("Roller Passive Speed", 0.1f));
 
-        bangBangControl(velocity, makeTrigger(), "Shooter").send(motors);
+        PauseTimer actuallyFiring = new PauseTimer(500);
+        // TODO: take robot modes into account
+        BooleanInput warmup = ZukoAzula.controlBinding.addBoolean("Shooter Warm-Up");
+        BooleanInput fire = ZukoAzula.controlBinding.addBoolean("Shooter Fire");
+        BooleanInput spinup = warmup.or(fire).or(actuallyFiring);
+
+        BooleanInput shouldSpinUp = setupRollersForSpinup(spinup, actuallyFiring);
+
+        shooter.setup(shouldSpinUp);
+
+        fire.and(shooter.isUpToSpeed).onPress().send(actuallyFiring);
     }
 
-    private static TalonExtendedMotor makeTalons() {
+    private static TalonExtendedMotor makeLinkedTalons() {
         TalonExtendedMotor talonLeft = FRC.talonCAN(10);
 
         TalonExtendedMotor talonRight = FRC.talonCAN(11);
@@ -39,16 +56,23 @@ public class Shooter {
         return talonLeft;
     }
 
-    private static BooleanInput makeTrigger() {
-        BooleanInput trigger = ZukoAzula.controlBinding.addBoolean("Shoot");
-        ArbitratedBoolean shooterTrigger = ZukoAzula.behaviors.addBoolean();
-        shooterTrigger.attach(ZukoAzula.teleop, trigger);
-        shooterTrigger.attach(ZukoAzula.pit, trigger);
-        return shooterTrigger;
+    private static BooleanInput setupRollersForSpinup(BooleanInput spinup, BooleanInput fire) throws ExtendedMotorFailureException {
+        BooleanInput interlude = quickPauseTimer(100, spinup.onPress());
+
+        // TODO: what if race condition?
+        Behavior spinupBehavior = rollerArb.addBehavior("Spin Up", spinup);
+        Behavior fireBehavior = rollerArb.addBehavior("Fire", fire);
+        Behavior interludeBehavior = rollerArb.addBehavior("Interlude", interlude);
+        rollerSpeed.attach(spinupBehavior, FloatInput.zero);
+        rollerSpeed.attach(fireBehavior, ZukoAzula.mainTuning.getFloat("Roller Fire Speed", 1f));
+        rollerSpeed.attach(interludeBehavior, ZukoAzula.mainTuning.getFloat("Roller Interlude Speed", -0.1f));
+
+        return rollerArb.getIsActive(spinupBehavior);
     }
 
-    private static FloatInput bangBangControl(FloatInput velocity, BooleanInput trigger, String name) {
-        BooleanInput isBelowVelocity = velocity.atMost(ZukoAzula.mainTuning.getFloat(name + " Target Velocity", 1));
-        return trigger.and(isBelowVelocity).toFloat(0.0f, ZukoAzula.mainTuning.getFloat(name + " Maximum Voltage", 1));
+    private static BooleanInput quickPauseTimer(int delay, EventInput onPress) {
+        PauseTimer pt = new PauseTimer(delay);
+        onPress.send(pt);
+        return pt;
     }
 }
