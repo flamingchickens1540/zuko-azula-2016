982c239a9476f6bff5c66e06180bbf004f8a32fb
Tytynguyen <tytynguyens2000@yahoo.com>
2016-02-07 13:24:55 -0800

    Fixed emoji

--- ZukoAzula2016/src/org/team1540/zukoazula/ZukoAzula.java
+++ ZukoAzula2016/src/org/team1540/zukoazula/ZukoAzula.java
@@ -26,7 +26,7 @@ public class ZukoAzula implements FRCApplication {
 
     @Override
     public void setupRobot() throws ExtendedMotorFailureException {
-        Logger.info("ðŸ�£ CHEEP CHEEP ðŸ�£");
+        Logger.info("🐣 CHEEP CHEEP 🐣");
 
         DriveCode.setup();
         Shooter.setup();
