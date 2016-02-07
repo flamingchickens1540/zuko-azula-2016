df0c388ca35842bab72ea91d5ac7e4cb63d0112e
Tytynguyen <tytynguyens2000@yahoo.com>
2016-02-07 00:17:01 -0800

    Implemented the auto-leveler with PID

--- ZukoAzula2016/src/org/team1540/zukoazula/Portcullis.java
+++ ZukoAzula2016/src/org/team1540/zukoazula/Portcullis.java
@@ -9,6 +9,7 @@ import ccre.channel.FloatIO;
 import ccre.channel.FloatInput;
 import ccre.cluck.Cluck;
 import ccre.ctrl.ExtendedMotorFailureException;
+import ccre.ctrl.PIDController;
 import ccre.drivers.ctre.talon.TalonExtendedMotor;
 import ccre.frc.FRC;
 
@@ -16,46 +17,31 @@ public class Portcullis {
 
     private static final TalonExtendedMotor leftGrabMotor = FRC.talonCAN(12);
     private static final TalonExtendedMotor rightGrabMotor = FRC.talonCAN(13);
-    
+
     private static final FloatIO leftEncoder = leftGrabMotor.modEncoder().getEncoderPosition();
     private static final FloatIO rightEncoder = rightGrabMotor.modEncoder().getEncoderPosition();
-    
+
     private static final FloatInput control = ZukoAzula.controlBinding.addFloat("Portcullis Grabber Axis").deadzone(0.2f);
-    private static final BooleanInput levelButton = ZukoAzula.controlBinding.addBoolean("Level Portcullis Arms");
 
     private static final ArbitratedFloat leftInput = ZukoAzula.behaviors.addFloat();
-    private static final ArbitratedFloat rightInput = ZukoAzula.behaviors.addFloat();  
-   
-    private static BooleanCell isLevel = new BooleanCell();
-    
-    public static void setup() throws ExtendedMotorFailureException{
-    Cluck.publish("Zero Portcullis Encoders", leftEncoder.eventSet(0).combine(rightEncoder.eventSet(0)));
-    Cluck.publish("Portcullis Left Angle", leftEncoder);
-    Cluck.publish("Portcullis Right Angle", rightEncoder);
-    Cluck.publish("Portcullis Arms are Level", isLevel);
-    
-    isLevel = (BooleanCell) leftEncoder.minus(rightEncoder).inRange(-10, 10); //TODO set correct range
+    private static final ArbitratedFloat rightInput = ZukoAzula.behaviors.addFloat();
 
-    leftInput.attach(ZukoAzula.teleop, control);
-    rightInput.attach(ZukoAzula.teleop, control);
-    leftInput.attach(ZukoAzula.pit, control);
-    rightInput.attach(ZukoAzula.pit, control);
-    
-    leftInput.send(leftGrabMotor.simpleControl().addRamping(0.1f, FRC.constantPeriodic));
-    rightInput.send(rightGrabMotor.simpleControl().addRamping(0.1f, FRC.constantPeriodic));
-    
-    EventOutput levelArms = new EventOutput(){ //TODO actually make this work
-        public void event(){
-           if(leftEncoder.get()<rightEncoder.get()){
-                while(!leftEncoder.minus(rightEncoder).inRange(-10, 10).get()){
-                    leftInput.send(ZukoAzula.mainTuning.getFloat("Portcullis Arm Level Speed", 0.4f));
-                }
-            }
-        }};
-    levelButton.onPress().send(levelArms);
+    public static void setup() throws ExtendedMotorFailureException {
+        Cluck.publish("Zero Portcullis Encoders", leftEncoder.eventSet(0).combine(rightEncoder.eventSet(0)));
+        Cluck.publish("Portcullis Left Angle", leftEncoder);
+        Cluck.publish("Portcullis Right Angle", rightEncoder);
+
+        PIDController pid = PIDController.createFixed(FRC.constantPeriodic, leftEncoder.asInput(), rightEncoder.asInput(), ZukoAzula.mainTuning.getFloat("Portcullis PID:P", 0.7f).get(), ZukoAzula.mainTuning.getFloat("Portcullis PID:I", 0.0f).get(), ZukoAzula.mainTuning.getFloat("Portcullis PID:D", 0f).get());
+        pid.setOutputBounds(0.3f);
+
+        leftInput.attach(ZukoAzula.teleop, control.plus(pid));
+        rightInput.attach(ZukoAzula.teleop, control.minus(pid));
+        leftInput.attach(ZukoAzula.pit, control.minus(pid));
+        rightInput.attach(ZukoAzula.pit, control);
+
+        leftInput.send(leftGrabMotor.simpleControl().addRamping(0.1f, FRC.constantPeriodic));
+        rightInput.send(rightGrabMotor.simpleControl().addRamping(0.1f, FRC.constantPeriodic));
 
     }
-   
 
-    
 }
