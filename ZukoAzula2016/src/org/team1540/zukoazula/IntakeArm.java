731b76ea29dd8e7dca337318014acc248dff5dfa
AidanSmith <smithai@catlin.edu>
2016-02-11 16:02:51 -0800

    Added encoder limits on the intake arm

--- ZukoAzula2016/src/org/team1540/zukoazula/IntakeArm.java
+++ ZukoAzula2016/src/org/team1540/zukoazula/IntakeArm.java
@@ -3,8 +3,10 @@ package org.team1540.zukoazula;
 import ccre.channel.BooleanIO;
 import ccre.channel.BooleanInput;
 import ccre.channel.EventInput;
+import ccre.channel.FloatCell;
 import ccre.channel.FloatInput;
 import ccre.channel.FloatOutput;
+import ccre.cluck.Cluck;
 import ccre.ctrl.ExtendedMotor;
 import ccre.ctrl.ExtendedMotorFailureException;
 import ccre.drivers.ctre.talon.TalonExtendedMotor;
@@ -17,6 +19,13 @@ public class IntakeArm {
     private static final FloatInput intakeArmAxis = ZukoAzula.controlBinding.addFloat("Intake Arm Axis").deadzone(0.2f).negated();
 
     public static void setup() throws ExtendedMotorFailureException {
-        intakeArmAxis.send(intakeArmCAN.simpleControl());
+        FloatCell armLow = ZukoAzula.mainTuning.getFloat("Intake Arm Low Position", 0f);
+        FloatCell armHigh = ZukoAzula.mainTuning.getFloat("Intake Arm High Position", 1f);
+        FloatInput armPosition = encoder.normalize(armLow, armHigh);
+        BooleanInput stop = armPosition.atLeast(1).and(intakeArmAxis.atLeast(0)).or(armPosition.atMost(0).and(intakeArmAxis.atMost(0)));
+        stop.toFloat(intakeArmAxis.multipliedBy(ZukoAzula.mainTuning.getFloat("Intake Arm Speed", .25f)), 0f).send(intakeArmCAN.simpleControl());
+
+        Cluck.publish("Intake Arm Set Low Position", () -> armLow.set(encoder.get()));
+        Cluck.publish("Intake Arm Set High Position", () -> armHigh.set(encoder.get()));
     }
 }
