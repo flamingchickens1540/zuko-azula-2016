e176e345eae11af198865f20fa56d1bfa076f413
AidanSmith <smithai@catlin.edu>
2016-02-13 17:18:50 -0800

    Reorganized my code and minor changes

--- ZukoAzula2016/src/org/team1540/zukoazula/IntakeArm.java
+++ ZukoAzula2016/src/org/team1540/zukoazula/IntakeArm.java
@@ -20,41 +20,34 @@ import ccre.drivers.ctre.talon.TalonExtendedMotor;
 import ccre.frc.FRC;
 import ccre.instinct.InstinctModule;
 import ccre.log.LogLevel;
-import ccre.timers.PauseTimer;
 
 public class IntakeArm {
     private static final TalonExtendedMotor intakeArmCAN = FRC.talonCAN(9);
     private static final FloatIO encoder = intakeArmCAN.modEncoder().getEncoderPosition();
     private static final FloatInput outputCurrent = intakeArmCAN.modFeedback().getOutputCurrent();
 
-    private static final FloatInput intakeArmAxis = ZukoAzula.controlBinding.addFloat("Intake Arm Axis").deadzone(0.2f).negated().multipliedBy(ZukoAzula.mainTuning.getFloat("Intake Arm Speed", .5f));
+    private static final FloatInput targetArmVelocity = ZukoAzula.controlBinding.addFloat("Intake Arm Axis").deadzone(0.2f).negated().multipliedBy(ZukoAzula.mainTuning.getFloat("Intake Arm Speed", .5f));
 
     private static final BooleanCell needsToCalibrate = new BooleanCell(true);
-
-    private static final FloatCell armHigh = ZukoAzula.mainTuning.getFloat("Intake Arm High Position", 1f);
 
     private static final BehaviorArbitrator armBehaviors = new BehaviorArbitrator("Intake Arm Behaviors");
     private static final ArbitratedFloat control = armBehaviors.addFloat();
 
+    private static final FloatInput passiveSpeed = ZukoAzula.mainTuning.getFloat("Intake Arm Counteract Gravity Speed", .1f);
+
     public static void setup() throws ExtendedMotorFailureException {
-        FloatInput armPosition = encoder.normalize(armHigh.minus(ZukoAzula.mainTuning.getFloat("Intake Distance Between Encoders", 2760)), armHigh.minus(ZukoAzula.mainTuning.getFloat("Intake Arm High Position Offset", 100)));
-        BooleanInput tooHigh = armPosition.atLeast(1).and(intakeArmAxis.atLeast(0));
-        BooleanInput tooLow = armPosition.atMost(0).and(intakeArmAxis.atMost(0));
+        FloatInput armPosition = encoder.normalize(ZukoAzula.mainTuning.getFloat("Intake Distance to Low Position", -2760), ZukoAzula.mainTuning.getFloat("Intake Distance to High Position", -100));
+        BooleanInput tooHigh = armPosition.atLeast(1).and(targetArmVelocity.atLeast(0));
+        BooleanInput tooLow = armPosition.atMost(0).and(targetArmVelocity.atMost(0));
         BooleanInput stop = tooHigh.or(tooLow);
 
-        control.attach(armBehaviors.addBehavior("teleop", FRC.inTeleopMode()), stop.toFloat(intakeArmAxis, 0f));
-
-        FloatInput counteractGravity = ZukoAzula.mainTuning.getFloat("Intake Arm Counteract Gravity Speed", .1f);
-        control.attach(armBehaviors.addBehavior("counteract gravity", armPosition.atMost(.5f).and(intakeArmAxis.absolute().atMost(counteractGravity))), counteractGravity);
-
         BooleanInput calibrating = needsToCalibrate.and(FRC.inTeleopMode().or(FRC.inAutonomousMode()));
-
-        control.attach(armBehaviors.addBehavior("calibrating", calibrating), ZukoAzula.mainTuning.getFloat("Intake Arm Speed During Calibration", .3f));
-
-        EventOutput calibrateArms = armHigh.eventSet(encoder).combine(needsToCalibrate.eventSet(false));
-
+        EventOutput calibrateArms = encoder.eventSet(0).combine(needsToCalibrate.eventSet(false));
         calibrateArms.on(outputCurrent.atLeast(ZukoAzula.mainTuning.getFloat("Intake Arm Stalling Current Threshold", 4)).onPress().and(calibrating));
 
+        control.attach(armBehaviors.addBehavior("teleop", FRC.inTeleopMode()), stop.toFloat(targetArmVelocity, 0f));
+        control.attach(armBehaviors.addBehavior("counteract gravity", armPosition.atMost(.5f).and(targetArmVelocity.inRange(FloatInput.zero, passiveSpeed))), passiveSpeed);
+        control.attach(armBehaviors.addBehavior("calibrating", calibrating), ZukoAzula.mainTuning.getFloat("Intake Arm Speed During Calibration", .3f));
         control.send(intakeArmCAN.simpleControl());
 
         EventLogger.log(calibrating.onPress(), LogLevel.INFO, "Started intake arm calibration");
