24074815a3cfb8a796d7227cc56e8be786808c60
Alexander Mackworth <alexander@tssr.co>
2016-03-04 17:33:54 -0800

    Renamed to ChallengeBrake.

--- ZukoAzula2016/src/org/team1540/zukoazula/ChallengeBrake.java
+++ ZukoAzula2016/src/org/team1540/zukoazula/ChallengeBrake.java
@@ -4,9 +4,9 @@ import ccre.channel.BooleanInput;
 import ccre.channel.FloatOutput;
 import ccre.frc.FRC;
 
-public class EmergencyBrake {
-    private static final BooleanInput firstTrigger = ZukoAzula.controlBinding.addBoolean("Emergency Brake A");
-    private static final BooleanInput secondTrigger = ZukoAzula.controlBinding.addBoolean("Emergency Brake B");
+public class ChallengeBrake {
+    private static final BooleanInput firstTrigger = ZukoAzula.controlBinding.addBoolean("Challenge Brake A");
+    private static final BooleanInput secondTrigger = ZukoAzula.controlBinding.addBoolean("Challenge Brake B");
     private static final BooleanInput composedTriggers = FRC.inTeleopMode().and(firstTrigger).and(secondTrigger);
 
     private static final FloatOutput servos = FRC.servo(0, -1, 1).combine(FRC.servo(1, -1, 1));
--- ZukoAzula2016/src/org/team1540/zukoazula/ZukoAzula.java
+++ ZukoAzula2016/src/org/team1540/zukoazula/ZukoAzula.java
@@ -21,6 +21,6 @@ public class ZukoAzula implements FRCApplication {
         Shooter.setup();
         Portcullis.setup();
         IntakeArm.setup();
-        EmergencyBrake.setup();
+        ChallengeBrake.setup();
     }
 }
